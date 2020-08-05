4bbaf278623f14c5286522ef057369426cc6f3d7
viniciussaturnino <viniciussaturnino07@hotmail.com>
2020-08-04 23:19:46 -0300

    Created connections routes and connections controller

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -87,6 +87,7 @@ export default class ClassesController {
             
             return res.status(201).send();
         } catch (err) {
+            console.log(err);
             await trx.rollback();
     
             return res.status(400).json({
