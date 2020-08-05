6241df084302466b0b3538950c2845a2cb8dd511
viniciussaturnino <viniciussaturnino07@hotmail.com>
2020-08-05 18:15:00 -0300

    Created select component

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
+import Textarea from '../../components/Textarea';
+import Select from '../../components/Select';
 import Input from '../../components/Input';
 
 import warningIcon from '../../assets/images/icons/warning.svg';
@@ -22,12 +24,29 @@ function TeacherForm() {
                     <Input name="name" label="Nome completo" />
                     <Input name="avatar" label="Avatar" />
                     <Input name="whatsapp" label="Whatsapp" />
+                    <Textarea name="bio" label="Biografia" />
                 </fieldset>
                 
                 <fieldset>
                     <legend>Sobre a aula</legend>
 
-                    <Input name="subject" label="Matéria" />
+                    <Select
+                        name="subject"
+                        label="Matéria"
+                        options={[
+                            { value: 'Matemática', label: 'Matemática' },
+                            { value: 'Física', label: 'Física' },
+                            { value: 'Química', label: 'Química' },
+                            { value: 'Biologia', label: 'Biologia' },
+                            { value: 'História', label: 'História' },
+                            { value: 'Português', label: 'Português' },
+                            { value: 'Geografia', label: 'Geografia' },
+                            { value: 'Inglês', label: 'Inglês' },
+                            { value: 'Sociologia', label: 'Sociologia' },
+                            { value: 'Filosofia', label: 'Filosofia' },
+                            { value: 'Artes', label: 'Artes' },
+                        ]}
+                    />
                     <Input name="cost" label="Custo da sua hora por aula" />
                 </fieldset>
 
