496b95d4158dde0bfcf921018bdcd6456e434271
viniciussaturnino <viniciussaturnino07@hotmail.com>
2020-08-13 23:05:51 -0300

    Finished style from register confirmation page

--- web/src/pages/RegisterConfirm/index.tsx
+++ web/src/pages/RegisterConfirm/index.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 
+import checkIcon from '../../assets/images/icons/success-check-icon.svg';
+
+import './styles.css';
+
 function RegisterConfirm() {
     return (
-        <h1>Register Confirmation</h1>
+        <div className="page-container">
+            <div className="confirm-content">
+                <img src={checkIcon} alt="check"/>
+                <h1>Cadastro salvo!</h1>
+            </div>
+            <p>Tudo certo, seu cadastro está na nossa lista de professores. Agora é só ficar de olho no seu WhatsApp.</p>
+            <button>Acessar lista</button>
+        </div>
     );
 }
 
