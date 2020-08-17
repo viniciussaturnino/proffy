815699ffadce6dca9fbeb48aebe95dd7cce85ee8
viniciussaturnino <viniciussaturnino07@hotmail.com>
2020-08-16 23:02:34 -0300

    List all teacher when search is without filter

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -17,26 +17,37 @@ export default class ClassesController {
         const week_day = filters.week_day as string;
         const time = filters.time as string;
 
+        let classes = [];
+
         if (!filters.week_day || !filters.subject || !filters.time) {
-            return res.status(400).json({
-                error: 'Missing filters to search classes.'
-            });
+            classes = await db('classes')
+                .whereExists(function() {
+                    this.select('class_schedule.*')
+                        .from('class_schedule')
+                })
+                .join('users', 'classes.user_id', '=', 'users.id')
+                .select(['classes.*', 'users.*']);
+            
+            // return res.status(400).json({
+            //     error: 'Missing filters to search classes.'
+            // });
         }
-
-        const timeInMinutes = convertHourToMinutes(time);
-
-        const classes = await db('classes')
-            .whereExists(function() {
-                this.select('class_schedule.*')
-                    .from('class_schedule')
-                    .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
-                    .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
-                    .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
-                    .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes])
-            })
-            .where('classes.subject', '=', subject)
-            .join('users', 'classes.user_id', '=', 'users.id')
-            .select(['classes.*', 'users.*']);
+        else {
+            const timeInMinutes = convertHourToMinutes(time);
+    
+            classes = await db('classes')
+                .whereExists(function() {
+                    this.select('class_schedule.*')
+                        .from('class_schedule')
+                        .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
+                        .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
+                        .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
+                        .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes])
+                })
+                .where('classes.subject', '=', subject)
+                .join('users', 'classes.user_id', '=', 'users.id')
+                .select(['classes.*', 'users.*']);
+        }
 
         return res.json(classes);
     }
